f53cceb8574e2fe1de895b50000570f4f77d87b2
urbanadventurer <horton.nz@gmail.com>
2020-04-22 15:28:48 +0400

    Implemented a mutex semaphore to fix a thread-safe bug with country IP lookups

--- country.rb
+++ country.rb
@@ -12,24 +12,24 @@ class Countrylookup
 
 def Countrylookup.startup
 # ok, set up @rfile. open once.
-	folder=File.expand_path(File.dirname(__FILE__))	
-	country_db = folder + "/country-ips.dat"
+folder=File.expand_path(File.dirname(__FILE__))	
+country_db = folder + "/country-ips.dat"
 
-	if File.exists?(country_db)
-		@rfile=File.open(country_db,"rb")
-	else
-		if File.exists?(folder + "/IpToCountry.csv")
+if File.exists?(country_db)
+  @rfile=File.open(country_db,"rb")
+else
+  if File.exists?(folder + "/IpToCountry.csv")
 		# pack that file & do it once
-			last_start=nil
-			last_end=nil
-			last_country=nil
-			File.open(folder + "/country-ips.dat","wb") do |wfile|
-				IO.foreach(folder + "/IpToCountry.csv") do |line|
-          next if line[0..0] =="#"
-					next if !(line =~ /^"/ )
-					s,e,d1,d2,co=line.delete!("\"").split(",")
-					s,e = s.to_i,e.to_i
-					if !last_start
+   last_start=nil
+   last_end=nil
+   last_country=nil
+   File.open(folder + "/country-ips.dat","wb") do |wfile|
+    IO.foreach(folder + "/IpToCountry.csv") do |line|
+      next if line[0..0] =="#"
+      next if !(line =~ /^"/ )
+      s,e,d1,d2,co=line.delete!("\"").split(",")
+      s,e = s.to_i,e.to_i
+      if !last_start
 						# initialize with first entry
 						last_start,last_end,last_country = s,e,co
 					else
@@ -53,23 +53,25 @@ def Countrylookup.startup
 		else
       raise "Aborting: Cannot find country database."
     end
-	end
+  end
 
-	f = folder + "/country-codes.txt"
-	@ccnames={};
-	File.open(f,"r:UTF-8").readlines.each do |line|
-		x=line.split(",");
-		@ccnames[x[1]] = x[0]
-	end
+  f = folder + "/country-codes.txt"
+  @ccnames={};
+  File.open(f,"r:UTF-8").readlines.each do |line|
+    x=line.split(",");
+    @ccnames[x[1]] = x[0]
+  end
 end
 
 
 def Countrylookup.ip2cc(ip)
   found=[]
 
-  if @rfile and ip and ip =~ /^([0-9]{1,3}\.){3}[0-9]{1,3}$/
-	  @rfile.seek(0,IO::SEEK_END)
-	  record_max=@rfile.pos/10-1
+  $semaphore_countrylookup.synchronize do
+
+    if @rfile and ip and ip =~ /^([0-9]{1,3}\.){3}[0-9]{1,3}$/
+      @rfile.seek(0,IO::SEEK_END)
+      record_max=@rfile.pos/10-1
 
 	    # build a 4-char string representation of IP address
 	    # in network byte order so it can be a string compare below
@@ -83,24 +85,25 @@ def Countrylookup.ip2cc(ip)
 	      str=@rfile.read(8)      # for range matching, we need only 8 bytes
 	      # at comparison, values are big endian, i.e. packed("N")
 	      if ipstr>=str[0,4]     # is this IP not below the current range?
-		if ipstr<=str[4,4]   # is this IP not above the current range?
-		  #puts  # a perfect match, voila!
-		  cc=@rfile.read(2)
-      found =[cc,@ccnames[cc]]
-		  break
-		else
-		  low=mid+1          # binary search: raise lower limit
-		end
-	      else
-		high=mid-1           # binary search: reduce upper limit
-	      end
+		      if ipstr<=str[4,4]   # is this IP not above the current range?
+      		  #puts  # a perfect match, voila!
+      		  cc=@rfile.read(2)
+            found = [cc,@ccnames[cc]]
+            break
+          else
+      		  low=mid+1          # binary search: raise lower limit
+      		end
+        else
+        	high=mid-1           # binary search: reduce upper limit
+        end
 	      if low>high            # no entries left? nothing found		
-  		#m << {:string=>"No country"}	
-		break
-	      end
-	    end
+  			#m << {:string=>"No country"}	
+        break
+        end
+      end
+    end
   end
-found
+  found
 end
 
 end
